e1b7e1dc314d224718b68bd2329c278f57322a8b
tomas.jerman <tomas.jerman@bafdefb5-79ff-3e4c-8193-be25f676ad87>
2017-04-21 19:09:56 +0000

    Agregado utils.js
    git-svn-id: https://buemrmfls01/svn/SITE2017/LOREAL/LORA254-Loreal-TV@1534 bafdefb5-79ff-3e4c-8193-be25f676ad87

---
+++ Source/resources/assets/js/utils.js
@@ -0,0 +1,29 @@
+export function fetch(url, options) {
+  const promise = new Promise(( resolve, reject)=>{
+    fetch(url, options)
+    .then((response)=>{
+      resolve(response);
+    })
+    .catch((error)=>{
+      reject(error);
+    });
+  });
+  return promise;
+}
+export function submit(method, url, options) {
+  const xhr = new XMLHttpRequest();
+  if (options.progressHandler) {
+    xhr.upload.addEventListener('progress', options.progressHandler, false);
+  }
+  if (options.loadHandler) {
+    xhr.upload.addEventListener('load', options.loadHandler, false);
+  }
+  if (options.overriveMimeTpye) {
+    xhr.overriveMimeTpye(options.overriveMimeTpye);
+  }
+  if (options.onreadyStateChange) {
+    xhr.onreadystatechange(options.onreadyStateChange);
+  }
+  xhr.open(method, url);
+  return xhr;
+}
