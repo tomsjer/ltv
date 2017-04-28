fd24463b7e362b3d65c2bba1448f02b1cd1f7042
tomas.jerman <tomas.jerman@bafdefb5-79ff-3e4c-8193-be25f676ad87>
2017-04-28 16:40:52 +0000

    Agregado Slideshow.jsx
    git-svn-id: https://buemrmfls01/svn/SITE2017/LOREAL/LORA254-Loreal-TV@1646 bafdefb5-79ff-3e4c-8193-be25f676ad87

---
+++ Source/resources/assets/js/components/Slideshow.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Slideshow extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Slideshow</h1>
+      </div>
+    );
+  }
+}
+
+export { Slideshow };
