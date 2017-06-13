cad172b2b9bedcd324edf9cf87926d530214b537
jesus.gonzalez2 <jesus.gonzalez2@bafdefb5-79ff-3e4c-8193-be25f676ad87>
2017-06-13 18:22:51 +0000

    Added panel view for Slideshow.jsx Added padding
    git-svn-id: https://buemrmfls01/svn/SITE2017/LOREAL/LORA254-Loreal-TV@2567 bafdefb5-79ff-3e4c-8193-be25f676ad87

--- Source/resources/assets/js/components/Slideshow.jsx
+++ Source/resources/assets/js/components/Slideshow.jsx
@@ -11,6 +11,7 @@ class Slideshow extends React.Component {
       activeSlide: 0
     };
   }
+
   render() {
     const slidesThumbnails = this.props.slides.map((slide, i)=>{
       return ( <li key={i} className={ (i === this.state.activeSlide) ? 'active' : '' }><img src={slide.srcThumbnail}/></li> );
@@ -18,21 +19,40 @@ class Slideshow extends React.Component {
 
     return (
       <div className="slideShow-container col-xs-12">
-        <div id="slideContainer">
-          <div id="activeSlide" className="row">
-            <div className="col-md-8">
-              <div className="row">
-                <Slide slide={ this.props.slides[this.state.activeSlide] } />
-              </div>
-              <div id="slideScroller"  className="row">
-                <div className="arrow left"><p>&lt;</p></div>
-                  <ul className="slidesThumbnails">
-                    { slidesThumbnails }
-                  </ul>
-                <div className="arrow right"><p>&gt;</p></div>
+        <div>
+          <ul className="nav nav-tabs" role="tablist">
+            <li role="presentation" className="active">
+              <a href="#slide" aria-controls="slide" role="tab" data-toggle="tab">
+                Slideshow
+              </a>
+            </li>
+            <li role="presentation">
+              <a href="#birthday" aria-controls="birthday" role="tab" data-toggle="tab">Cumpleaños</a>
+            </li>
+          </ul>
+          <div className="tab-content">
+            <div role="tabpanel" className="tab-pane active" id="slide">
+              <div id="slideContainer">
+                <div id="activeSlide" className="row">
+                  <div className="col-md-8">
+                    <div className="row">
+                      <Slide slide={ this.props.slides[this.state.activeSlide] } />
+                    </div>
+                    <div id="slideScroller"  className="row">
+                      <div className="arrow left"><p>&lt;</p></div>
+                      <ul className="slidesThumbnails">
+                          { slidesThumbnails }
+                      </ul>
+                      <div className="arrow right"><p>&gt;</p></div>
+                    </div>
+                  </div>
+                  <SlideForm slide={ this.props.slides[this.state.activeSlide] } />
+                </div>
               </div>
             </div>
-            <SlideForm slide={ this.props.slides[this.state.activeSlide] } />
+            <div role="tabpanel" className="tab-pane" id="birthday">
+              <h1>Cumpleaños</h1>
+            </div>
           </div>
         </div>
       </div>
