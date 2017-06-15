45a20b5a6a3d51cf40d762a932e5205382d60ade
jesus.gonzalez2 <jesus.gonzalez2@bafdefb5-79ff-3e4c-8193-be25f676ad87>
2017-06-15 17:10:51 +0000

    Updated to show time on head
    git-svn-id: https://buemrmfls01/svn/SITE2017/LOREAL/LORA254-Loreal-TV@2634 bafdefb5-79ff-3e4c-8193-be25f676ad87

--- Source/resources/views/layouts/app.blade.php
+++ Source/resources/views/layouts/app.blade.php
@@ -53,6 +53,9 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <li class="time">
+                                <a></a>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" id="dropdownMenu">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -84,5 +87,6 @@
     <!-- Scripts -->
     <script src="https://apis.google.com/js/api.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/time.js') }}"></script>
 </body>
 </html>
