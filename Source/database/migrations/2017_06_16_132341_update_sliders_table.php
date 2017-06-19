650afe558c350eff91f0718bb7e79f0f4eea3f58
jesus.gonzalez2 <jesus.gonzalez2@bafdefb5-79ff-3e4c-8193-be25f676ad87>
2017-06-19 15:24:51 +0000

    Changed sliders table
    git-svn-id: https://buemrmfls01/svn/SITE2017/LOREAL/LORA254-Loreal-TV@2724 bafdefb5-79ff-3e4c-8193-be25f676ad87

--- Source/database/migrations/2017_06_16_132341_update_sliders_table.php
+++ Source/database/migrations/2017_06_16_132341_update_sliders_table.php
@@ -16,12 +16,7 @@ class UpdateSlidersTable extends Migration
         Schema::table('sliders', function (Blueprint $table) {
             $table->text('title')->nullable();
             $table->text('subtitle')->nullable();
-            $table->string('img_src',250)->nullable();
-            $table->string('thumbnail_src',250)->nullable();
-            $table->string('video_id',250)->nullable();
             $table->integer('video_loop')->unsigned()->nullable();
-            $table->integer('media_type_id')->unsigned();
-            $table->foreign('media_type_id')->references('id')->on('media_type');
             $table->text('description')->nullable()->change();
         });
     }
@@ -33,14 +28,10 @@ class UpdateSlidersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('sliders', function (Blueprint $table) {
             $table->dropColumn('title');
             $table->dropColumn('subtitle');
-            $table->dropColumn('img_src');
-            $table->dropColumn('thumbnail_src');
-            $table->dropColumn('video_id');
             $table->dropColumn('video_loop');
-            $table->dropColumn('media_type_id');
             $table->string('description',50)->change();
         });
     }
